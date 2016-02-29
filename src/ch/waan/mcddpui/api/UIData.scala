8c21ca7659befc8e81900952881d1c889f1ebf82
Andreas Wälchli <andreas.waelchli@me.com>
2016-02-29 17:29:35 +0100

    implemented data structure base types for UI applications

---
+++ src/ch/waan/mcddpui/api/UIData.scala
@@ -0,0 +1,22 @@
+package ch.waan.mcddpui.api
+
+import scala.collection.immutable.HashMap
+
+/**
+ * root object for the structural representation of the
+ * UI state in a UI application. Contains a view stack
+ * and an immutable hash map for representing global UI
+ * state.
+ *
+ * @author Andreas Waelchli <andreas.waelchli@me.com>
+ * @version 1.1 (0.1.0), 2016-02-29
+ * @since MCDDPUI 0.1.0
+ *
+ * @param viewStack the stack containing the view data
+ * 				representations in the order they are
+ * 				layered. (opaque layers!).
+ * @param props a hash map containing global UI properties
+ */
+case class UIData(
+    viewStack: List[ViewData],
+    props: HashMap[String, String] = HashMap.empty)
---
+++ src/ch/waan/mcddpui/api/UIUniverse.scala
@@ -0,0 +1,19 @@
+package ch.waan.mcddpui.api
+
+/**
+ * data structure root node for UI applications.
+ *
+ * Holds the operational data as well as an abstract
+ * representation encoding the state of the UI. This
+ * allows for full historizing not only operational
+ * data but for UI state changes as well.
+ *
+ * @author Andreas Waelchli <andreas.waelchli@me.com>
+ * @version 1.1 (0.1.0), 2016-02-29
+ * @since MCDDPUI 0.1.0
+ *
+ * @tparam T the data type of the operational data
+ * @param data the operational data
+ * @param ui the UI state representation
+ */
+case class UIUniverse[T](data: T, ui: UIData)
